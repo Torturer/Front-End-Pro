beeaffa65bf9441bd2d274fd978db4939193f4ed
Torturer <94932039+Torturer@users.noreply.github.com>
2022-04-14 12:15:23 +0300

    Optimization and fix other bug

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -13,35 +13,27 @@ function Separator(text) {
         firstText = firstTextPartOne + firstTextPartTwo;
         secondText = secondTextPartOne + secondTextPartTwo;
 
-        // console.log(`first text part one ${firstTextPartOne}`)
-        // console.log(`first text part two ${firstTextPartTwo}`)
-        // console.log(`second text part one ${secondTextPartOne}`)
-        // console.log(`second text part two ${secondTextPartTwo}`)
-
-        return result = firstText + secondText;
+        return firstText + secondText;
     }
     first = text[0].toUpperCase();
     second = text.slice(1).toLowerCase();
-    return result = first + second;
+    return first + second;
 }
 
 UserName = prompt(`Please, enter your name`);
-
-if ((UserName == ``) || (UserName == null)) {
+if (!UserName) {
     UserName = `Sergy`;
 } else UserName = Separator(UserName.trim());
 
 SurName = prompt(`Please, enter your surname`);
-
-if ((SurName == ``) || (SurName == null)) {
+if (!SurName) {
     SurName = `Shpora`;
 } else SurName = Separator(SurName.trim());
 
 FullName = UserName + ` ` + SurName;
 
 UserMail = prompt(`Please, enter your email adress`);
-
-if ((UserMail == ``) || (UserMail == null)) {
+if (!UserMail) {
     UserMail = `email not defined`
 } else {
     UserMail = UserMail.replaceAll(` `, ``).toLowerCase();
@@ -50,12 +42,12 @@ if ((UserMail == ``) || (UserMail == null)) {
     } else if (UserMail.startsWith(`@`)) {
         UserMail = `not valid email <span class="info-warn">${UserMail}</span> (symbol @ find in first place)`;
     } else if (UserMail.endsWith(`@`)) {
-        UserMail = `not valid email <span class="info-warn">${email}</span> (symbol @ find in last place)`;
+        UserMail = `not valid email <span class="info-warn">${UserMail}</span> (symbol @ find in last place)`;
     }
 }
 
 UserBirthYear = prompt(`Please enter your birthday year`);
-if ((UserBirthYear == ``) || (UserBirthYear == null)) {
+if (!UserBirthYear) {
     UserBirthYear = 1997;
 } else {
     UserBirthYear = parseInt(UserBirthYear.replaceAll(` `, ``));
