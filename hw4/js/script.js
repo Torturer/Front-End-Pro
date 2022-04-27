5538ec44628c15552d9479bf1427336d28c489d6
Torturer <94932039+Torturer@users.noreply.github.com>
2022-04-28 00:56:15 +0300

    Update script.js

--- hw4/js/script.js
+++ hw4/js/script.js
@@ -1,24 +1,9 @@
-// debugger;
 if (confirm(`Tell me three most important words 💚`)) {
     wordOne = ValidWork(Processor(1));
     wordTwo = ValidWork(Processor(2));
     wordThree = ValidWork(Processor(3));
     console.log(`${wordOne}` + ` ${wordTwo}` + ` ${wordThree}!`)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //functions 
 function Processor(index) {
@@ -29,19 +14,24 @@ function Processor(index) {
 }
 
 function ValidWork(object) {
-    debugger;
+    // debugger;
     do {
-        text = prompt(`Choose type of transformation for type text: uppercase || lowercase || capitalize`, `capitalize`)
-    } while (!text && !text.includes(`uppercase`) && !text.includes(`lowercase`) && !text.includes(`capitalize`))
-    switch (text) {
-        case `uppercase`:
+        type = prompt(`Choose type of transformation for type text: uppercase - 1 || lowercase - 2 || capitalize - 3`, `3`)
+    } while (!type || type.length > 1 || !Test(type))
+    switch (type) {
+        case `1`:
             object.toUpperCase();
             break;
-        case `lowercase`:
+        case `2`:
             object.toLowerCase();
             break;
-        case `capitalize`:
+        case `3`:
             object = object[0].toUpperCase() + object.slice(1).toLowerCase();
     }
     return object;
+}
+
+function Test (number) {
+    if(number.includes(`1`) || number.includes(`2`) || number.includes(`3`)) return true;
+    else return false;
 }
