d474347e7d5d5f41c2de2bc00c6b3a5f77e9658e
Torturer <94932039+Torturer@users.noreply.github.com>
2022-05-25 11:41:50 +0300

    оптимизация
    упростил максимально код, из-за тех задачи касательно Math.abs пришлось создать два дополнительных метода для правильной работы, хотя сама операция math.abs из-за исключающего условия для размера массива.
    
    с применением math.abs код закомментирован.

--- hw5/js/script.js
+++ hw5/js/script.js
@@ -1,20 +1,20 @@
 arr = [];
 
-arr.length = FixeSizeArray(EnterNumber(`Pleas enter size array`, 2, 10))
-minRandomNumber = FixeRandomNumber(EnterNumber(`Please enter minimal number`, -10, 10));
-maxRandomNumber = FixeRandomNumber(EnterNumber(`Please enter maximal number`, minRandomNumber, 50));
+arr.length = EnterNumber(`Pleas enter size array`, 2, 10)
+minRandomNumber = EnterNumber(`Please enter minimal number`, -10, 10);
+maxRandomNumber = EnterNumber(`Please enter maximal number`, minRandomNumber, 50);
 
-for(i=0, sum = 0; i < arr.length; i++){
+for (i = 0, sum = 0; i < arr.length; i++) {
     arr[i] = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber);
     if (i == 0) {
         sum += arr[i];
         continue;
     }
-    sum*= arr[i]
+    sum *= arr[i]
 }
 
-console.log (arr);
-console.log (sum);
+console.log(arr);
+console.log(sum);
 
 //#region Functions 
 
@@ -22,18 +22,50 @@ function EnterNumber(text, min, max) {
     do {
         variable = +prompt(text + ` "${min}" - "${max}"`)
     } while (!variable || typeof variable != `number` || variable < min || variable > max)
+    if (!Number.isInteger(variable)) return Math.round(variable);
     return variable;
 }
 
-function FixeSizeArray(number) {
-    if (number < 0) number = Math.abs(number); // никогда не сработает при проверке в строке номер 24, но по условию тз написал)
-    if (!Number.isInteger(number)) number = Math.round(number);
-    return number;
-}
-
-function FixeRandomNumber(number) {
-    if (!Number.isInteger(number)) return Math.round(number);
-    return number;
-}
-
 //#endregion
+
+
+// код по тз с обязательным условием Math.abs
+// arr = [];
+
+// arr.length = FixeSizeArray(EnterNumber(`Pleas enter size array`, 2, 10))
+// minRandomNumber = FixeRandomNumber(EnterNumber(`Please enter minimal number`, -10, 10));
+// maxRandomNumber = FixeRandomNumber(EnterNumber(`Please enter maximal number`, minRandomNumber, 50));
+
+// for(i=0, sum = 0; i < arr.length; i++){
+//     arr[i] = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber);
+//     if (i == 0) {
+//         sum += arr[i];
+//         continue;
+//     }
+//     sum*= arr[i]
+// }
+
+// console.log (arr);
+// console.log (sum);
+
+// //#region Functions 
+
+// function EnterNumber(text, min, max) {
+//     do {
+//         variable = +prompt(text + ` "${min}" - "${max}"`)
+//     } while (!variable || typeof variable != `number` || variable < min || variable > max)
+//     return variable;
+// }
+
+// function FixeSizeArray(number) {
+//     if (number < 0) number = Math.abs(number); // никогда не сработает при проверке и входных данных в строке номер 56, но по условию тз написал)
+//     if (!Number.isInteger(number)) number = Math.round(number);
+//     return number;
+// }
+
+// function FixeNumber(number) {
+//     if (!Number.isInteger(number)) return Math.round(number);
+//     return number;
+// }
+
+// //#endregion
