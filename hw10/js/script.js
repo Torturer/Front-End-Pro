5d74f4f23fce56c2886ddf1acdcc7dc34dbcff3c
Torturer <94932039+Torturer@users.noreply.github.com>
2022-06-21 16:48:23 +0300

    complete hw 10

---
+++ hw10/js/script.js
@@ -0,0 +1,54 @@
+//#region  HW10 arguments
+var users = [
+    ["john", "red", 5, ["ball", "book", "pen"]],
+    ["becky", "blue", 10, ["tape", "backpack", "pen"]],
+    ["susy", "red", 55, ["ball", "eraser", "pen"]],
+    ["tyson", "green", 1, ["book", "pen"]],
+];
+//#endregion
+
+//#region HW Body 
+let score = 0;
+let firstArr = [];
+users
+    .forEach(function (item) {
+        firstArr.push(item[0] + `!`);
+        if (item[1] === `red`) {
+            score += item[2]
+        }
+    })
+let secondArr = users
+    .map(function (item) {
+        return item[0] + `?`
+    })
+
+let table = users
+    .filter(item => { return item[1] === `red` })
+    .map(user => {
+        return `<tr>${user
+            .map(item => `<td>${Array.isArray(item) ? item.join(`; `) : item}</td>`).join(``)}</tr>`
+    }).join(``)
+
+
+//#endregion
+
+document.write(`
+<table border="1">
+<thead>
+    <tr>
+        <th>Name</th>
+        <th>Comand</th>
+        <th>Score</th>
+        <th>Data</th>
+    </tr>
+</thead>
+<tbody>
+${table}
+</tbody>
+<tfoot>
+    <tr>
+        <td colspan="4">Total score: ${score}</td>
+    </tr>
+</tfoot>
+</table> 
+`)
