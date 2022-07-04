e70adbb222b953cfdd27cea57a834dcf0bc46158
Torturer <94932039+Torturer@users.noreply.github.com>
2022-07-05 00:03:41 +0300

    complete hw15

---
+++ hw15/js/script.js
@@ -0,0 +1,20 @@
+const body = document.querySelector(`body`)
+let square = document.createElement(`div`);
+
+square.className = `block`;
+body.append(square);
+
+//////
+
+setInterval(() => {
+    square.style.background = randomRGB()
+}, 500)
+
+setInterval(() => {
+    square.style.top = randomPX(window.innerHeight)
+    square.style.left = randomPX(window.innerWidth)
+}, 1000)
+
+const randomPX = (temp) => `${Math.floor(Math.random() * (temp-100))}px`
+
+const randomRGB = () => `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
